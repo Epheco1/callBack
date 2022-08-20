979c175acb06cf1a937ef2deca49e8e66bde0996
Epheco1 <Epheco1@WEMAITS>
2022-08-20 22:01:14 +0100

    Add project files.

---
+++ callBack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CallBack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SquareClass.SquareCalculator(Completed);
+        }
+        public static void Completed(int valuesCalculated)
+        {
+            Console.WriteLine($"square of {valuesCalculated} calculated");
+        }
+    }
+}
---
+++ callBack/callBackClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallBack
+{
+    internal static class SquareClass
+    {
+        public delegate void Square(int number);
+        public static void SquareCalculator(Square hf)
+        {
+            List<int> squaresList;
+            for (int i = 0; i <= 4000; i++)
+            {
+                var square = i * i;
+                squaresList = new List<int>(square);
+                hf(i);
+            }
+        }
+    }
+}
